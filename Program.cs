282a6ca592e290693ddfa56f4c6fe74a235f6496
André Gabriel Henrique do Nascimento Romano <andreg_h_donascimento@yahoo.com.br>
2024-07-24 19:29:56 -0300

    readme.md added

--- Program.cs
+++ Program.cs
@@ -17,26 +17,26 @@ if (!DateTime.TryParseExact(mesinput,"MM",null,System.Globalization.DateTimeStyl
 int dia=dianascimento.Day;
 int mes=mesnascimento.Month;
 if((dia>=21&&mes==3)||(dia<=20&&mes==4))
-Console.WriteLine("Você é de Aries.");
+    Console.WriteLine("Você é de Aries.");
 if((dia>=24&&mes==9)||(dia<=23&&mes==10))
-Console.WriteLine("Você é de Libra.");
+    Console.WriteLine("Você é de Libra.");
 if((dia>=21&&mes==4)||(dia<=21&&mes==5))
-Console.WriteLine("Você é de Touro.");
+    Console.WriteLine("Você é de Touro.");
 if((dia>=24&&mes==10)||(dia<=22&&mes==11))
-Console.WriteLine("Você é de Escorpião.");
+    Console.WriteLine("Você é de Escorpião.");
 if((dia>=22&&mes==5)||(dia<=21&&mes==6))
-Console.WriteLine("Você é de Gêmeos.");
+    Console.WriteLine("Você é de Gêmeos.");
 if((dia>=23&&mes==11)||(dia<=21&&mes==12))
-Console.WriteLine("Você é de Sagitário.");
+    Console.WriteLine("Você é de Sagitário.");
 if((dia>=21&&mes==6)||(dia<=23&&mes==7))
-Console.WriteLine("Você é de Câncer.");
+    Console.WriteLine("Você é de Câncer.");
 if((dia>=22&&mes==12)||(dia<=20&&mes==1))
-Console.WriteLine("Você é de Capricórnio.");
+    Console.WriteLine("Você é de Capricórnio.");
 if((dia>=24&&mes==7)||(dia<=23&&mes==8))
-Console.WriteLine("Você é de Leão.");
+    Console.WriteLine("Você é de Leão.");
 if((dia>=21&&mes==1)||(dia<=19&&mes==2))
-Console.WriteLine("Você é de Aquário.");
+    Console.WriteLine("Você é de Aquário.");
 if((dia>=24&&mes==8)||(dia<=23&&mes==9))
-Console.WriteLine("Você é de Virgem.");
+    Console.WriteLine("Você é de Virgem.");
 if((dia>=20&&mes==2)||(dia<=20&&mes==3))
-Console.WriteLine("Você é de Peixes.");
+    Console.WriteLine("Você é de Peixes.");
---
+++ readme.md
@@ -0,0 +1,20 @@
+Signos Astrológicos e suas Datas:
+
+Áries: 21 de março a 19 de abril
+Touro: 20 de abril a 20 de maio
+Gêmeos: 21 de maio a 20 de junho
+Câncer: 21 de junho a 22 de julho
+Leão: 23 de julho a 22 de agosto
+Virgem: 23 de agosto a 22 de setembro
+Libra: 23 de setembro a 22 de outubro
+Escorpião: 23 de outubro a 21 de novembro
+Sagitário: 22 de novembro a 21 de dezembro
+Capricórnio: 22 de dezembro a 19 de janeiro
+Aquário: 20 de janeiro a 18 de fevereiro
+Peixes: 19 de fevereiro a 20 de março
+
+Requisitos:
+
+Solicite ao usuário a data de nascimento.
+Determine o signo astrológico com base na data de nascimento.
+Exiba o signo astrológico no console.
